8829ad3d7f7b11ca0543209ce28d792ce1d66702
Hazy Cora <hazycora@gmail.com>
2021-03-15 02:19:52 -0500

    once again

--- index.js
+++ index.js
@@ -183,7 +183,7 @@ app.get('/search', async (req, res) => {
     if (req.query.q) {
       $( '#info' ).attr( 'style', 'display: none;' )
       $('#searchBar').attr('value', req.query.q)
-      ytsr(req.query.q).then(function(data) => {
+      ytsr(req.query.q).then(function(data) {
         let l = 10
         if (data.items.length < 10) {
           l = data.items.length
