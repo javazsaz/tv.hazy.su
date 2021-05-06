8a1c8c83eccd005db6d646068bab2cd871ad0302
hazycora <72580505+hazycora@users.noreply.github.com>
2021-05-06 15:19:33 -0500

    fix heroku problems

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 const fs = require('fs')
-require('dotenv').config()
 require('./js/routes')()
+require('./js/config')()
 require('./js/logs').start()
-if (!fs.existsSync('config.json')) {
-  console.log('No config.json found. Please set one up according to the README.')
-  process.exit()
-}
