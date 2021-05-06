94f250e1176f415939659fd7e1841886f46bca80
hazycora <72580505+hazycora@users.noreply.github.com>
2021-05-06 15:00:06 -0500

    use example config if potentially could be running on heroku

--- js/config.js
+++ js/config.js
@@ -5,7 +5,13 @@ function getConfig() {
   if (fs.existsSync(__dirname+'/../config.json')) {
     return JSON.parse(fs.readFileSync(__dirname+'/../config.json', 'utf8'))
   } else {
-    console.log('No config.json found. Please set one up according to the README.')
-    process.exit()
+    if (process.env.PORT) {
+      console.log('This may be running on Heroku, as indicated by PORT environment variable.')
+      console.log('To ease deployment for Heroku, using config.example.json for config.')
+      return JSON.parse(fs.readFileSync(__dirname+'/../config.example.json', 'utf8'))
+    } else{
+      console.log('No config.json found. Please set one up according to the README.')
+      process.exit()
+    }
   }
 }
