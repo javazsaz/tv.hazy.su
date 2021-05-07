a5aedfda6d4995b8515ebd3962294662268c4d55
hazycora <72580505+hazycora@users.noreply.github.com>
2021-05-07 11:33:10 -0500

    Bring back trending to homepage

--- js/pages/home.js
+++ js/pages/home.js
@@ -4,10 +4,11 @@ const fs = require('fs')
 const config = require('../config')()
 const cheerio = require('cheerio')
 const ytpl = require('ytpl')
+const ytrend = require("yt-trending-scraper")
 
 
 function genPage(req, res, next) {
-  fs.readFile('html/home/index.html', 'utf8', function(err, data){
+  fs.readFile('html/home/index.html', 'utf8', async function(err, data){
     if (err) {
       console.log(err)
       res.status(500).send('server error.')
@@ -15,38 +16,49 @@ function genPage(req, res, next) {
     }
     const $ = cheerio.load(data)
 
-    ytpl(config.homePlaylist).then(function(playlist) {
-      playlist.items.reverse()
-
-      for (let i = 0; i < playlist.items.length; i ++) {
-        let video = `
-
-        <div class="vid">
+    let playlist = await ytpl(config.homePlaylist)
+    playlist.items.reverse()
+    for (let i = 0; i < playlist.items.length; i ++) {
+      let video = `
+      <div class="vid">
+        <a href="watch?v=${playlist.items[i].id}">
+          <img class="thumb" src="https://i.ytimg.com/vi/${playlist.items[i].id}/hqdefault.jpg">
+        </a>
+        <div class="metadata">
           <a href="watch?v=${playlist.items[i].id}">
-            <img class="thumb" src="https://i.ytimg.com/vi/${playlist.items[i].id}/hqdefault.jpg">
+            <p class="title">${playlist.items[i].title}</p>
           </a>
-          <div class="metadata">
-            <a href="watch?v=${playlist.items[i].id}">
-              <p class="title">${playlist.items[i].title}</p>
-            </a>
-            <a href="/creator/${playlist.items[i].author.channelID}">
-              <p class="creator">${playlist.items[i].author.name}</p>
-            </a>
-            <p class="smallData">${playlist.items[i].duration}</p>
-          </div>
+          <a href="/creator/${playlist.items[i].author.channelID}">
+            <p class="creator">${playlist.items[i].author.name}</p>
+          </a>
+          <p class="smallData">${playlist.items[i].duration}</p>
         </div>
-        `
+      </div>
+      `
+      $( '#suggestBar' ).append( video )
+    }
+    let trending = await ytrend.scrape_trending_page('US', false)
+    for (let i = 0; i < trending.length; i ++) {
+      let video = `
+      <div class="vid">
+        <a href="watch?v=${trending[i].videoId}">
+          <img class="thumb" src="https://i.ytimg.com/vi/${trending[i].videoId}/hqdefault.jpg">
+        </a>
+        <div class="metadata">
+          <a href="watch?v=${trending[i].videoId}">
+            <p class="title">${trending[i].title}</p>
+          </a>
+          <a href="/creator/${trending[i].authorId}">
+            <p class="creator">${trending[i].author}</p>
+          </a>
+          <p class="smallData">${trending[i].timeText}</p>
+        </div>
+      </div>
+      `
+      $( '#usTrendBar' ).append( video )
+    }
 
-        $( '#suggestBar' ).append( video )
-      }
-
-      res.send($.html())
-
-
-    }).catch(function (err) {
-      res.json(err)
-      console.log(err)
-    })
+    res.send($.html())
 
   })
 }
